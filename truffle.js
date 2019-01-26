252113ab7826814eef8f99f5582c8552464b4341
Travis Wilson <wilson.travis.t@gmail.com>
2019-01-26 12:39:57 -0500

    Fixed seperators for eslint

--- truffle.js
+++ truffle.js
@@ -1,22 +1,22 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const fs = require('fs')
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
 
 // First read in the secrets.json to get our mnemonic
-let secrets
-let mnemonic
+let secrets;
+let mnemonic;
 if (fs.existsSync('secrets.json')) {
-  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
-  mnemonic = secrets.mnemonic
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+  mnemonic = secrets.mnemonic;
 } else {
   console.log('No secrets.json found. If you are trying to publish EPM ' +
-              'this will fail. Otherwise, you can ignore this message!')
-  mnemonic = ''
+              'this will fail. Otherwise, you can ignore this message!');
+  mnemonic = '';
 }
 
 module.exports = {
   networks: {
     live: {
-      network_id: 1 // Ethereum public network
+      network_id: 1, // Ethereum public network
       // optional config values
       // host - defaults to "localhost"
       // port - defaults to 8545
@@ -26,17 +26,22 @@ module.exports = {
     },
     ropsten: {
       provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
-      network_id: '3'
+      network_id: '3',
     },
     testrpc: {
-      network_id: 'default'
+      network_id: 'default',
     },
     coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,        
+      host: 'localhost',
+      network_id: '*',
+      port: 8555,
       gas: 0xfffffffffff,
-      gasPrice: 0x01     
+      gasPrice: 0x01,
     },
-  }
-}
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
+  },
+};
